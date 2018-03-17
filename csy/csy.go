88899c6b466c7d06ad329c6d61aaeb10bfc97e21
chensongyi <chensongyi@x-donkey.com>
2018-03-18 00:12:49 +0800

    0.0.1

---
+++ csy/csy.go
@@ -0,0 +1,9 @@
+package csy
+
+import (  
+    "fmt"
+)
+
+func Say() {
+    fmt.Println("SONIC CHEN IS ONLINE")
+}
---
+++ hello/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import ( 
+    "fmt"
+    "github.com/snail200x/go/string"
+    "github.com/snail200x/go/csy"
+)
+
+func main() {
+    fmt.Println("hello, new gopher!")
+    fmt.Println(string.Reverse("hello, new gopher!"))
+    csy.Say()
+}
